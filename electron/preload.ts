b41c6d098d574194fda8769d0183ada6a78ac914
Mananwebdev160408 <guptamanan576@gmail.com>
2026-07-28 15:44:39 +0530

    feat(electron): implement security headers and preload context bridge

---
+++ electron/preload.ts
@@ -0,0 +1,52 @@
+import { contextBridge, ipcRenderer } from 'electron';
+
+contextBridge.exposeInMainWorld('purgoAPI', {
+  // Window controls
+  minimizeWindow: () => ipcRenderer.invoke('purgo:minimize'),
+  maximizeWindow: () => ipcRenderer.invoke('purgo:maximize'),
+  closeWindow: () => ipcRenderer.invoke('purgo:close'),
+  isMaximized: () => ipcRenderer.invoke('purgo:isMaximized'),
+
+  // Filesystem scan
+  scanDirectory: (dirPath: string, options?: any) =>
+    ipcRenderer.invoke('purgo:scanDirectory', dirPath, options),
+
+  // Cache scan
+  scanCaches: () =>
+    ipcRenderer.invoke('purgo:scanCaches'),
+
+  // Trash management
+  moveToTrash: (sourcePath: string, projectName: string, folderName: string, sizeBytes: number) =>
+    ipcRenderer.invoke('purgo:moveToTrash', sourcePath, projectName, folderName, sizeBytes),
+
+  restoreFromTrash: (id: string) =>
+    ipcRenderer.invoke('purgo:restoreFromTrash', id),
+
+  deletePermanently: (id: string) =>
+    ipcRenderer.invoke('purgo:deletePermanently', id),
+
+  emptyTrash: () =>
+    ipcRenderer.invoke('purgo:emptyTrash'),
+
+  getTrashManifest: () =>
+    ipcRenderer.invoke('purgo:getTrashManifest'),
+
+  purgePathPermanently: (targetPath: string) =>
+    ipcRenderer.invoke('purgo:purgePathPermanently', targetPath),
+
+  setRetentionDays: (days: number) =>
+    ipcRenderer.invoke('purgo:setRetentionDays', days),
+
+  // System
+  getHomeDir: () =>
+    ipcRenderer.invoke('purgo:getHomeDir'),
+
+  selectFolder: () =>
+    ipcRenderer.invoke('purgo:selectFolder'),
+
+  onScanProgress: (callback: (path: string) => void) =>
+    ipcRenderer.on('purgo:scanProgress', (_event, path) => callback(path)),
+
+  removeScanProgressListener: () =>
+    ipcRenderer.removeAllListeners('purgo:scanProgress'),
+});
